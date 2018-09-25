66468ac3833f46cd2302e4551a497de018ba1ff0
aquakks <aquakks7@gmail.com>
2018-09-26 01:24:45 +0200

    BookInfo/Authors

---
+++ src/BookInfo/Authors.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+const title = localStorage.getItem('book');
+
+export default class Authors extends React.Component {
+  state = {
+    authors : []
+  }
+
+
+
+
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/books/`+title + '/authors')
+    .then(res => {
+        const authors = res.data;
+        this.setState({ authors });
+      })
+  }
+
+
+  render() {
+    return (
+        <ul>
+        { this.state.authors.map(authors => <li>{authors.name}</li>)}
+        { this.state.authors.map(authors => <li>{authors.surname}</li>)}
+      </ul>
+
+    )
+  }
+}
--- src/BookInfo/BookInfo.jsx
+++ src/BookInfo/BookInfo.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
 import axios from 'axios';
+import Authors from './Authors'
+
 
 export default class BookInfo extends React.Component {
   state = {
     book : [],
     year : '',
     title : '',
-    publicationdate : ''
+    publicationdate : '',
+    img : ''
   }
-
-
 
   componentDidMount() {
     axios.get(`http://localhost:5000/books/`+this.props.match.params.id)
       .then(res => {
+        localStorage.setItem('book', this.props.match.params.id);
         const book = res.data;
         this.setState({ 
           book : book.year,
           title : book.title,
           year : book.year,
-          name : book.name });
+          name : book.name, 
+          img : "../" + book.title + ".jpeg" });
       })
   }
 
+
   render() {
     return (
-      <ul>
+      <div>
       {this.state.book}
       {this.state.year}
       {this.state.title}
-      </ul>
+      {this.state.img}
+      
+      <h1>Image</h1>
+
+
+      <Authors />
+
+</div>
     )
   }
 }
