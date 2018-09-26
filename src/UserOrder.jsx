28f1728e758d0c7ace446c239ce934d366bea13a
aquakks <aquakks7@gmail.com>
2018-09-26 21:11:06 +0200

    Dziala

--- src/UserOrder.jsx
+++ src/UserOrder.jsx
@@ -14,20 +14,17 @@ export class UserOrder extends Component {
         ext : '',
       };
 
+
+
       componentDidMount() {
-        //axios.get('http://localhost:5000/borrows/' + id + '/all')
-        axios.get('http://localhost:5000/borrows/32845f07-ac16-43de-9ea8-26529d70747a/all')
-          .then(res => {
-            const borrows = res.data;
-            this.setState({ 
-            bookId : borrows.bookId,
-            startDate : borrows.startDate,
-            endDate: borrows.endDate,
-            penalty : borrows.penalty,
-            ext : borrows.extends
-          });
- 
-      } )
+        fetch('http://localhost:5000/borrows/' + id + '/all')
+        .then(res => res.json())
+        .then(json => {
+            let result = json.results || json;
+            this.setState({ books: result });
+            console.log(result)
+          }
+          );
     }
 
      render() {
